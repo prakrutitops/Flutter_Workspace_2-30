8aa339b9ec7eedb0378727c5583bb08d11c7a1c8
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-11-29 15:39:50 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,73 +1,102 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 
-void main() {
-runApp(new MyApp());
-}
-
-class MyApp extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-	return new MaterialApp(
-	home: new GalleryAccess(),
-	debugShowCheckedModeBanner: false,
-	);
-}
+void main()
+{
+  runApp(MyApp());
 }
 
-class GalleryAccess extends StatefulWidget {
-@override
-State<StatefulWidget> createState() {
-	return new GalleryAccessState();
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(home: MyPaymentPage());
+  }
+
 }
+class MyPaymentPage extends StatefulWidget {
+  const MyPaymentPage({super.key});
+
+  @override
+  State<MyPaymentPage> createState() => _MyPaymentPageState();
 }
 
-class GalleryAccessState extends State<GalleryAccess> {
-File galleryFile;
-
-@override
-Widget build(BuildContext context) {
-	//display image selected from gallery
-	imageSelectorGallery() async {
-	galleryFile = await ImagePicker.pickImage(
-		source: ImageSource.gallery,
-		// maxHeight: 50.0,
-		// maxWidth: 50.0,
-	);
-	setState(() {});
-	}
-
-	return new Scaffold(
-	appBar: new AppBar(
-		title: new Text('Gallery Access'),
-		backgroundColor: Colors.green,
-		actions: <Widget>[
-		Text("GFG",textScaleFactor: 3,)
-		],
-	),
-	body: new Builder(
-		builder: (BuildContext context) {
-		return Center(
-			child: new Column(
-			mainAxisAlignment: MainAxisAlignment.center,
-			children: <Widget>[
-				new RaisedButton(
-				child: new Text('Select Image from Gallery'),
-				onPressed: imageSelectorGallery,
-				),
-				SizedBox(
-				height: 200.0,
-				width: 300.0,
-				child: galleryFile == null
-					? Center(child: new Text('Sorry nothing selected!!'))
-					: Center(child: new Image.file(galleryFile)),
-				)
-			],
-			),
-		);
-		},
-	),
-	);
+class _MyPaymentPageState extends State<MyPaymentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
-}
+
+// class MyPaymentPage extends StatefulWidget
+// {
+//   @override
+//   State<MyPaymentPage> createState() => _MyPaymentPageState();
+// }
+//
+// class _MyPaymentPageState extends State<MyPaymentPage>
+// {
+//   late Token _paymentToken;
+//   late PaymentMethod _paymentMethod;
+//   late String _error;
+//   final String _currentSecret = 'YOUR_STRIPE_SECRET_KEY';
+//
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//
+//     StripePayment.setOptions(
+//       StripeOptions(
+//         publishableKey: 'YOUR_STRIPE_PUBLISHABLE_KEY',
+//         merchantId: 'Test',
+//         androidPayMode: 'test',
+//       ),
+//     );
+//   }
+//
+//
+//   Future<void> makepayment() async
+//   {
+//     StripePayment.createPaymentMethod(
+//       PaymentMethodRequest(card: CreditCard()),
+//     ).then((paymentMethod) {
+//       setState(() {
+//         _paymentMethod = paymentMethod;
+//       });
+//       // Use _paymentMethod to make the payment
+//     }).catchError(setError);
+//   }
+//   void setError(dynamic error) {
+//     setState(()
+//     {
+//       _error = error.toString();
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context)
+//   {
+//     return Scaffold(appBar: AppBar(title: Text("Payment ntegration")),
+//       body: Column(
+//
+//         children: [
+//           ElevatedButton(
+//             onPressed: ()
+//             {
+//               makepayment();
+//             },
+//             child: Text("Make Payment"),),
+//
+//           if (_error != null) Text('Error: $_error'),
+//         ],
+//       ) ,
+//
+//
+//     );
+//   }
+// }
+//
+//
+//
+//
